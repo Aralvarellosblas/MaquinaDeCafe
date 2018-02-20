8fdf8bc5762cf849e34ff744a4ff12d016e9a66f
artur <artur@ArturoMSI>
2018-02-20 13:50:45 +0100

    Javadoc para la clase Display

--- src/maquinacafe/Display.java
+++ src/maquinacafe/Display.java
@@ -3,16 +3,30 @@ package maquinacafe;
 import javax.swing.JOptionPane;
 
 /**
+ * Clase con los metodos para mostrar mensajes a traves de pantalla
  *
  * @author Arturo
  */
-public class Display {
+public class Display{
+
+    /**
+     * Metodo que muestra un mensaje por pantalla
+     *
+     * @param mensaje mensaje a moostrar
+     */
     public static void mostrarMensaje(String mensaje){
         JOptionPane.showMessageDialog(null, mensaje);
     }
+
+    /**
+     * Metodo que pide un dato lo recoje y lo devuleve
+     *
+     * @param mensaje mensaje que se muestra
+     * @return dato recogido
+     */
     public static String mensajePedir(String mensaje){
         String valor=JOptionPane.showInputDialog(mensaje);
         return valor;
     }
-    
+
 }
