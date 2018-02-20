a78f5c6a2dc9cd4143344c3f300a1abc7b61d05f
arturo <arturo@ArturoMSI>
2018-02-20 13:49:10 +0100

    Javadoc para la clase Botonera

--- src/maquinacafe/Botonera.java
+++ src/maquinacafe/Botonera.java
@@ -3,16 +3,23 @@ package maquinacafe;
 import java.util.ArrayList;
 
 /**
+ * Clase con los botones de interaccion necesarios para la interaccion con el
+ * programa
  *
  * @author Arturo
  */
 public class Botonera{
-   
+
     private static Producto prod=null;
     private static boolean val=false;
-    
+
+    /**
+     * Metodo que muestra los botones disponibles y se encarga de llamar a los
+     * metodos necesario de las otras clases y para el funcionamiento de la
+     * accion seleccionada por el usuario
+     */
     public static void botones(){
-        
+
         int selec;
         do{
             selec=Integer.parseInt(Display.mensajePedir("Escoja una de las siguientes opciones:\n"
@@ -23,7 +30,7 @@ public class Botonera{
                     +"0-- Salir del programa\n"
                     +"\nCredito actual: "+Monedero.getDinero()));
             switch(selec){
-                case 1: 
+                case 1:
                     Monedero.introducirDinero();
                     break;
                 case 2:
@@ -40,8 +47,9 @@ public class Botonera{
                     break;
                 case 0:
                     break;
-                default: Display.mostrarMensaje("Debe seleccionar uno de los anteriores");
-                
+                default:
+                    Display.mostrarMensaje("Debe seleccionar uno de los anteriores");
+
             }
         }while(selec!=0);
         Display.mostrarMensaje("Que tenga un buen día");
