d52a1637c573b08cea7265ea01b7ebd2d2ae90f5
artur <artur@ArturoMSI>
2018-02-20 11:42:31 +0100

    Clase Main Terminada

---
+++ src/maquinacafe/MaquinaCafe.java
@@ -0,0 +1,16 @@
+package maquinacafe;
+
+/**
+ *
+ * @author Arturo
+ */
+public class MaquinaCafe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Botonera.botones();
+    }
+
+}
