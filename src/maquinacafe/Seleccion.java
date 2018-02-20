5d08746aa44f9a0b41dd5ea5807d20a9ea41f91d
arturo <arturo@ArturoMSI>
2018-02-20 13:51:50 +0100

    Javadoc para la clase Seleccion

--- src/maquinacafe/Seleccion.java
+++ src/maquinacafe/Seleccion.java
@@ -2,15 +2,23 @@ package maquinacafe;
 
 import java.util.ArrayList;
 
-
 /**
+ * Clase con los metodos necesarios para elegir un producto y
+ * fabricarlo
  *
  * @author Arturo
  */
 public class Seleccion{
 
-    private static  Producto prod;
+    private static Producto prod;
 
+    /**
+     * Metodo que nos permite escoger un producto de los disponibles en la
+     * maquina
+     *
+     * @param p un ArrayList con los productos de la maquina
+     * @return el producto elegido por el usuario
+     */
     public static Producto seleccionarProducto(ArrayList<Producto> p){
         String sel=Display.mensajePedir("Elegir un Producto: "
                 +"\nCafe"
@@ -25,18 +33,34 @@ public class Seleccion{
         return prod;
     }
 
+    /**
+     * Metodo que elabora el producto seleccionado por el usuario si se ha
+     * introducido el dinero suficiente y si no muestra el precio de dicho
+     * producto
+     *
+     * @param pr producto selecionado por el usuario
+     * @param v variable de validacion del credito
+     */
     public static void elaborarSeleccion(Producto pr, boolean v){
         if(v==true){
             v=false;
             Display.mostrarMensaje("Elaborando el producto: "+pr.getNombre()+"\nPor favor Espere\n"
-                    + "******\n"
-                    + "******\n"
-                    + "******\n"
-                    + "Producto terminado");
+                    +"******\n"
+                    +"******\n"
+                    +"******\n"
+                    +"Producto terminado");
             Monedero.devolverCambio(pr.getPrecio());
-        }else Seleccion.mostrarPrecio(pr);
-        
+        }else{
+            Seleccion.mostrarPrecio(pr);
+        }
+
     }
+
+    /**
+     * Metodo que muestra el precio de un producto determinado
+     *
+     * @param pr producto selecionado por el usuario
+     */
     public static void mostrarPrecio(Producto pr){
         Display.mostrarMensaje("Valor de producto: "+pr.getNombre()+", "+pr.getPrecio()+"€");
     }
